a87a2e4095c63d37f5a1ddcfc6575730d7ccdc4e
ABazire <bazire.arthur@gmail.com>
2026-08-20 23:43:02 +0200

    Update page.tsx

--- src/app/(game)/coffre/page.tsx
+++ src/app/(game)/coffre/page.tsx
@@ -27,7 +27,9 @@ export default async function CoffrePage() {
         </button>
       </form>
 
-      <form action={ouvrirCoffre}>
+      <form action={async () => {
+  await ouvrirCoffre(...);
+}}>
         <button
           type="submit"
           className={styles.openButton}
